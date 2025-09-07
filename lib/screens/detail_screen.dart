f5cb229137581a109647e11b447c582a13c14d06
StoneEngine <stkingner@gmail.com>
2025-09-07 12:55:07 +0700

    Third Screen

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  static String routeName = '/detail';
+  // final String message;
+  const DetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final args =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>;  
+    return Scaffold(
+      appBar: AppBar(title: const Text("Detail Screen")),
+      body: Center(
+        child: Column(
+          children: [
+            Text('Item id: ${args['itemId']}'),
+            Text("Detail: ${args['message']}"),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context, 'item1 detail returned.');
+              },
+              child: Text("Go Back "),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:navigate_route/screens/detail_screen.dart';
+import 'package:navigate_route/screens/third_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  static String routeName = '/home';
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                final result = await Navigator.pushNamed(
+                  context,
+                  DetailScreen.routeName,
+
+                  arguments: {
+                    'itemId': 'Peter Denman',
+                    'message': 'smart person',
+                  },
+                );
+                print("ได้ค่ากลับมาคือ $result");
+              },
+              child: const Text('Go to Detail Screen.'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final result = await Navigator.pushNamed(
+                  context,
+                  ThirdScreen.routeName,
+
+                  arguments: {'itemId': 'Third Item', 'message': 'black'},
+                );
+                print("ได้ค่ากลับมาคือ $result");
+              },
+              child: const Text('Go to Third Screen.'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
